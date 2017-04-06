2e837ced72468f956668fc12a2d8c17f985972a9
Nizul Zaim <mnizulzaim@gmail.com>
2017-04-06 22:26:10 +0800

    Ensure Directory Exist First

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 let pathJoin = require("path.join");
-let  Jimp = require("jimp");
+let Jimp = require("jimp");
 let fs = require('fs');
+let fse = require('fs-extra');
 
-module.exports.resizeCrop = function(inputFolder = "./input", outputFolder ="./output", w = 50, h = 50, q = 100) {
+function checkDirectory(directory, callback) {
+    fs.stat(directory, function (err, stats) {
+        //Check if error defined and the error code is "not exists"
+        if (err && err.errno === 34) {
+            //Create the directory, call the callback.
+            fs.mkdir(directory, callback);
+        } else {
+            //just in case there was a different error:
+            callback(err)
+        }
+    });
+}
+
+module.exports.resizeCrop = function (inputFolder = "./input", outputFolder = "./output", w = 50, h = 50, q = 100) {
     var files = fs.readdirSync(inputFolder);
-    files.forEach(function(file) {
-        let inputPath = pathJoin(inputFolder, file);
-        let outputPath = pathJoin(outputFolder, file);
-        Jimp.read(inputPath, function (err, image) {
-            if (err) throw err;
-            image.cover( w, h ).quality(q).write(outputPath); 
-        });
-    })
+
+    fse.ensureDir(outputFolder, err => {
+        if (err) {
+            console.log(err) 
+            return;
+        }
+        files.forEach(function (file) {
+            let inputPath = pathJoin(inputFolder, file);
+            let outputPath = pathJoin(outputFolder, file);
+            Jimp.read(inputPath, function (err, image) {
+                if (err) throw err;
+                image.cover(w, h).quality(q).write(outputPath);
+            });
+        })
+    });
 }
