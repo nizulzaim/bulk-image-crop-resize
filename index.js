6779bfdd4e599c2facaa16bb160bc72a7f105270
Nizul Zaim <mnizulzaim@gmail.com>
2017-04-06 22:27:09 +0800

    clean code

--- index.js
+++ index.js
@@ -3,19 +3,6 @@ let Jimp = require("jimp");
 let fs = require('fs');
 let fse = require('fs-extra');
 
-function checkDirectory(directory, callback) {
-    fs.stat(directory, function (err, stats) {
-        //Check if error defined and the error code is "not exists"
-        if (err && err.errno === 34) {
-            //Create the directory, call the callback.
-            fs.mkdir(directory, callback);
-        } else {
-            //just in case there was a different error:
-            callback(err)
-        }
-    });
-}
-
 module.exports.resizeCrop = function (inputFolder = "./input", outputFolder = "./output", w = 50, h = 50, q = 100) {
     var files = fs.readdirSync(inputFolder);
 
